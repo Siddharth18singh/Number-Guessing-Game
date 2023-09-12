71fe27d282b8fb4bfb9cab9650244ce9ffd97314
Siddharth18singh <144835943+Siddharth18singh@users.noreply.github.com>
2023-09-12 23:29:13 +0530

    Add files via upload

---
+++ Number guessing game .c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int number_to_guess, user_guess, attempts = 0;
+    srand(time(NULL)); // Seed the random number generator
+
+    // Generate a random number between 1 and 100
+    number_to_guess = rand() % 100 + 1;
+
+    printf("Welcome to the Number Guessing Game!\n");
+    printf("I have selected a random number between 1 and 100. Try to guess it.\n");
+
+    do {
+        printf("Enter your guess: ");
+        scanf("%d", &user_guess);
+        attempts++;
+
+        if (user_guess < number_to_guess) {
+            printf("Too low! Try again.\n");
+        } else if (user_guess > number_to_guess) {
+            printf("Too high! Try again.\n");
+        } else {
+            printf("Congratulations! You guessed the correct number (%d) in %d attempts!\n", number_to_guess, attempts);
+        }
+    } while (user_guess != number_to_guess);
+
+    return 0;
+}
